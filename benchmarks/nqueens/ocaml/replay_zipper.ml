207cd7c7fe36eacc88d5b29d72d0c75ad0a4d6ba
Gabriel Scherer <gabriel.scherer@gmail.com>
2018-03-14 08:57:49 +0100

    replay: use a zipper (two stacks)
    The replay-based implementation using a zipper is sensibly more
    efficient than the one based on a `pos` reference within a branch
    index list, and much closer to the full thermometers implementation.
    
    I am also adding a version where `choose` takes an array rather than
    a list, to save on indexing costs there as well.

---
+++ benchmarks/nqueens/ocaml/replay_zipper.ml
@@ -0,0 +1,73 @@
+open List;;
+
+let past = ref []
+let future = ref []
+
+exception Empty;;
+
+let push r x = (r := x :: !r)
+let pop r = match !r with
+  | [] -> None
+  | x::xs -> r := xs; Some x
+
+let rec decr = function
+  | (0::ns) -> decr ns
+  | (n::ns) -> (n-1)::ns
+  | []      -> []
+
+let withNondeterminism f =
+  let rec loop acc f =
+    let v = try [f()] with Empty -> [] in
+    let acc = v @ acc in
+    assert (!future = []);
+    let next = List.rev (decr !past) in
+    future := next;
+    past := [];
+    if next = []
+    then List.rev acc
+    else loop acc f
+  in
+  past := [];
+  future := [];
+  loop [] f
+
+let choose = function
+  | [] -> raise Empty
+  | xs ->
+    match pop future with
+    | Some idx ->
+      push past idx;
+      nth xs ((length xs - 1) - idx)
+    | None ->
+      let idx = (length xs - 1) in
+      push past idx;
+      hd xs
+
+let fail () = choose []
+
+let n = int_of_string Sys.argv.(1)
+
+let range =
+  let rec range i =
+    if i = n then []
+    else i :: range (i + 1)
+  in range 0
+
+let rec okay i c = function
+  | [] -> true
+  | x::xs -> c <> x && (c-x) <> i && (c-x) <> -i && okay (i+1) c xs
+
+let rec enum_nqueens i l =
+  if i = n then
+    l
+  else begin
+    let c = choose range in
+    if not (okay 1 c l) then fail();
+    enum_nqueens (i + 1) (c :: l)
+  end
+
+let solutions = withNondeterminism (fun () -> enum_nqueens 0 [])
+
+let () =
+  print_int (List.length solutions);
+  print_newline ()
