ce8ed10260b28560ef5b6fad4fd05af9eeb4707c
Gabriel Scherer <gabriel.scherer@gmail.com>
2018-03-14 17:31:57 +0100

    improve benchmark scripts

--- benchmarks/nqueens/run_benchmark.rb
+++ benchmarks/nqueens/run_benchmark.rb
@@ -1,7 +1,7 @@
 #!/usr/local/bin/ruby
 file = ARGV[0]
 
-LOWERBOUND = 4
+LOWERBOUND = 8
 
 TRIALS = 5
 
@@ -15,13 +15,18 @@ end
 puts "script\t\"#{file}\""
 
 unless ARGV[2].nil?
-  puts "header\t\"#{ARGV[2]}\""
+  puts "header\t#{ARGV[2]}"
 end
 
 LOWERBOUND.upto(UPPERBOUND) do |n|
-  tot = 0.0
-  TRIALS.times { tot += `sml #{file} #{n}`.lines[-1].to_i }
-  avg = 1 + tot / TRIALS
-  puts "point\t#{n}\t#{avg}"
+  best = 0
+  TRIALS.times {
+    unless best > 60*1000*2 then
+      run = `timeout 10m sml #{file} #{n}`.lines[-1].to_i
+      if best = 0 || run < best then best = run end
+    end
+  }
+  best += 1 # stabilize on log-axis
+  puts "point\t#{n}\t#{best}"
 end
 
--- benchmarks/nqueens/run_benchmark_time.rb
+++ benchmarks/nqueens/run_benchmark_time.rb
@@ -5,7 +5,7 @@ require 'open3'
 
 cmd = ARGV[0]
 
-LOWERBOUND = 4
+LOWERBOUND = 8
 
 TRIALS = 5
 
@@ -19,20 +19,23 @@ end
 puts "script\t\"#{cmd}\""
 
 unless ARGV[2].nil?
-  puts "header\t\"#{ARGV[2]}\""
+  puts "header\t#{ARGV[2]}"
 end
 
 LOWERBOUND.upto(UPPERBOUND) do |n|
-  tot = 0.0
+  best = 0
 
   TRIALS.times do
-     _, stderr, _ = Open3.capture3("bash -c \"time #{cmd} #{n}\"")
-    s = stderr.lines.join
-    #p s
-    m = s.match(/user\s+(\d+)m(\d+.\d+)s/)
-    tot += (m[1].to_i * 60 + m[2].to_f) * 1000
+    unless best > 60*1000*2 then
+       _, stderr, _ = Open3.capture3("bash -c \"time #{cmd} #{n}\"")
+      s = stderr.lines.join
+      #p s
+      m = s.match(/user\s+(\d+)m(\d+.\d+)s/)
+      run = (m[1].to_i * 60 + m[2].to_f) * 1000
+      if best = 0 || run < best then best = run end
+    end
   end
-  avg = 1 + tot / TRIALS
-  puts "point\t#{n}\t#{avg}"
+  best += 1 # stabilize on log-axis
+  puts "point\t#{n}\t#{best}"
 end
 
